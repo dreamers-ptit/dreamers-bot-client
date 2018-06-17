a66c30ca5906c9cd375396ca1773688071bee0b4
thangnh <thang.nguyenhuu91@gmail.com>
2018-06-18 01:11:53 +0700

    create layout

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DasboardComponent } from './dasboard/dasboard.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+    { path: 'home', component: HomeComponent },
+    { path: 'dasboard', component: DasboardComponent },
+
+    { path: '**', redirectTo: '/home' }
+];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(routes, { useHash: true })],
+    exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
